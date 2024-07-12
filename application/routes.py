2c9ab5bebe4fc6ba53b57bdbf1ffe121dd1b07c0
rohitrk06 <rohitkumar.rk0600@gmail.com>
2024-07-12 11:55:23 +0530

    session update - 2

--- application/routes.py
+++ application/routes.py
@@ -44,3 +44,47 @@ def logout():
     session.pop('role', None)
     flash('You have been logged out')
     return redirect(url_for('index'))
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method=='GET':
+        return render_template('register.html')
+    if request.method=='POST':
+        username = request.form['username']
+        password = request.form['password']
+        confirm_password = request.form['confirm_password']
+        email = request.form['email']
+        address = request.form['address']
+        role = request.form['role']
+
+        if not username or not password or not email or not role:
+            flash('Please enter username, password, email and role')
+            return redirect(url_for('register'))
+
+        if len(password) < 8:
+            flash('Password must be at least 8 characters')
+            return redirect(url_for('register'))
+        
+        if password != confirm_password:
+            flash('Passwords do not match')
+            return redirect(url_for('register'))
+
+        if role not in ['admin', 'store_manager', 'customer']:
+            flash('Invalid role')
+            return redirect(url_for('register'))
+        
+        user = User.query.filter_by(username=username).first()
+        if user:
+            flash('User already exists')
+            return redirect(url_for('register'))
+        
+        user = User.query.filter_by(email=email).first()
+        if user:
+            flash('Email already exists')
+            return redirect(url_for('register'))
+        role = Role.query.filter_by(name=role).first()
+        new_user = User(username=username, password=password, email=email, address=address, roles = [role])
+        db.session.add(new_user)
+        db.session.commit()
+        flash('User registered successfully')
+        return redirect(url_for('login'))
